d441fdb3c58b586773009bab9b622c2147476d8f
sm7654 <115954704+sm7654@users.noreply.github.com>
2024-11-10 16:25:14 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,11 +1,17 @@
+
+
+
+
+
+
+
+//Button Controller
 const Buttonwraper = document.getElementsByClassName("Buttonwraper");
 const ControlButtons = document.getElementsByClassName("control-button");
-
 function Activate(i) {
     document.getElementsByClassName("navHelp")[i].classList.add('navHelpShow');
 
 }
-
 for (let i = 0; i < Buttonwraper.length; i++) {
     let HoverTimeout;
     Buttonwraper[i].addEventListener('mouseover', function(event) {
@@ -24,8 +30,6 @@ for (let i = 0; i < Buttonwraper.length; i++) {
     });
 }
 
-
-
 for (let i =0; i< ControlButtons.length; i++) {
     ControlButtons[i].addEventListener('click', function(event) {
         for (let j = 0; j  < ControlButtons.length; j++) {
@@ -40,103 +44,59 @@ for (let i =0; i< ControlButtons.length; i++) {
     });
 }
 
-class Experiment {
-    #name = "";
-    #date = "";
-    constructor(name) {
-        this.#name = name;
-        
-        const currentDate = new Date();
-        const year = currentDate.getFullYear();
-        const month = String(currentDate.getMonth() + 1);
-        const day = String(currentDate.getDate());
-        
-        this.#date = `${year}-${month}-${day}`;
-        
-    }
-
-    GetName() {
-        return this.#name;
-    }
-    GetDate() {
-        return this.#date
-    }
-}
-
-for (let i = 0; i < 10; i++) {
-    const Exper = new Experiment("Spoiler");
-    const ExperHTML = 
-    `
-    <aside class="experimentWraper">
-        <p class="contentText">${Exper.GetName()}</p>
-        <p class="contentText">${Exper.GetDate()}</p>
-    </aside>
-    `;
-    document.getElementsByClassName("HistoryofExperWraper")[0].insertAdjacentHTML("beforeend", ExperHTML);
-    
-}
-
-function ToggleSwitches() {
-    document.getElementsByClassName("onOffSwitches")[0].classList.toggle("ActiveSwitchStatus");
-    document.getElementsByClassName("onOffSwitches")[1].classList.toggle("ActiveSwitchStatus");
-}
-document.getElementById("hh").addEventListener('click', ToggleSwitches);
 
 
 
 
-
-
+// Slider Switch
 document.getElementById("Range").value = 0;
-document.getElementById("Range").addEventListener('click', function(event) {
-    try{
-        clearInterval(interval);
-    } catch (error) {console.log("no interval!")}
-    let x = document.getElementById("Range").value;
-    if (x < 99) {
-        let i = x;
+document.getElementById("Range").addEventListener('touchend', sliderCheck);
+document.getElementById("Range").addEventListener('mouseup', sliderCheck);
 
-        interval = setInterval(function() {
-            document.getElementById("Range").value = i - 1;
-            i--;
-            if (i < 1) {
+let interval;
+function sliderCheck() {
+    
+    console.log("mouse up!");
+    let x = document.getElementById("Range").value;
+    let counter = 0;
+    if (x < 99) {
+
+        interval = setInterval(() => {
+            counter++;
+            x -= 1;
+            document.getElementById("Range").value = x;
+            
+            if (x < 1) {
                 clearInterval(interval);
             }
-        }, 5);
+
+        }, 1);
+    
     } else {
         ToggleSwitches();
     }
-});
-let isThrottling = false;
+    
+}
 
-document.getElementById("Range").addEventListener('mousemove', function(event) {
-    if (isThrottling) return;
 
-    let rangeOffset = document.getElementById("Range").getBoundingClientRect();
-    var mouseXoffset = event.clientX - rangeOffset.left;
-    var mouseYoffset = event.clientY - rangeOffset.top;
 
-    document.getElementById("Range").style.backgroundPosition = `-${mouseXoffset}px -${mouseYoffset}px`;
 
-    isThrottling = true;
-    setTimeout(() => {
-        isThrottling = false;
-    }, 16); // Update every 16ms (~60fps)
-});
 
+
+
+//Onload showcase
 window.onload = function() {
     let counter = 0;
     const interval = setInterval(function() {
         counter++;
         let xIndex = Math.floor(Math.random() * (window.innerWidth  + 1));;
         let yIndex = Math.floor(Math.random() * (window.innerHeight + 1));
-        console.log(xIndex, yIndex)
-    
+        
         let wind = `        
-            <svg class="WindShow" style="top: ${yIndex}px; left: ${xIndex}px;" fill="#000000" viewBox="0 0 24 24" id="wind" xmlns="http://www.w3.org/2000/svg" class="icon multi-color"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><title style="stroke-width: 2;">wind</title><path id="secondary-stroke" d="M10,3a2,2,0,0,1,0,4m0,0H3M3,19h7" style="fill: none; stroke: #2ca9bc; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"></path><path id="primary-stroke" d="M17.5,11a3.5,3.5,0,0,0,0-7m0,7H3m13,4H3m13,6a3,3,0,0,0,0-6" style="fill: none; stroke: #000000; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"></path></g></svg>
-        `
+            <svg class="WindShow" style="top: ${yIndex}px; left: ${xIndex - 70}px;" fill="#000000" viewBox="0 0 24 24" id="wind" xmlns="http://www.w3.org/2000/svg" class="icon multi-color"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><title style="stroke-width: 2;">wind</title><path id="secondary-stroke" d="M10,3a2,2,0,0,1,0,4m0,0H3M3,19h7" style="fill: none; stroke: #2ca9bc; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"></path><path id="primary-stroke" d="M17.5,11a3.5,3.5,0,0,0,0-7m0,7H3m13,4H3m13,6a3,3,0,0,0,0-6" style="fill: none; stroke: #000000; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"></path></g></svg>
+        `;
         document.getElementsByClassName("openingShowWraper")[0].insertAdjacentHTML('beforeend', wind);
-
+        
         if (counter > 500) {
             clearInterval(interval);
             setTimeout(()=> {
@@ -147,5 +107,60 @@ window.onload = function() {
             }, 5000);
         }
     }, 10);
+    
 
+
+
+
+
+
+
+
+
+    // Temp stuff
+    
+    class Experiment {
+        #name = "";
+        #date = "";
+        constructor(name) {
+            this.#name = name;
+            
+            const currentDate = new Date();
+            const year = currentDate.getFullYear();
+            const month = String(currentDate.getMonth() + 1);
+            const day = String(currentDate.getDate());
+            
+            this.#date = `${year}-${month}-${day}`;
+            
+        }
+    
+        GetName() {
+            return this.#name;
+        }
+        GetDate() {
+            return this.#date
+        }
+    }
+    
+    for (let i = 0; i < 10; i++) {
+        const Exper = new Experiment("Spoiler");
+        const ExperHTML = 
+        `
+        <aside class="experimentWraper">
+            <p class="contentText">${Exper.GetName()}</p>
+            <p>_</p>
+            <p class="contentText">${Exper.GetDate()}</p>
+        </aside>
+        `;
+        document.getElementsByClassName("HistoryofExperWraper")[0].insertAdjacentHTML("beforeend", ExperHTML);
+        
+    }
+    
+    function ToggleSwitches() {
+        document.getElementsByClassName("onOffSwitches")[0].classList.toggle("ActiveSwitchStatus");
+        document.getElementsByClassName("onOffSwitches")[1].classList.toggle("ActiveSwitchStatus");
+    }
+    document.getElementById("hh").addEventListener('click', ToggleSwitches);
+    
+    
 }
